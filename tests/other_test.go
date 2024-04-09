6ac472457dd5fd24a9771522a190386d2ed201c4
Rajat Agrawal <rajatatibm@gmail.com>
2024-04-09 11:14:27 +0100

    test: fixed other_tests

--- tests/other_test.go
+++ tests/other_test.go
@@ -1,6 +1,8 @@
 package test
 
 import (
+	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testhelper"
+	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,7 +11,12 @@ import (
 func TestRunBasicExample(t *testing.T) {
 	t.Parallel()
 
-	options := setupOptions(t, "appid-bas", basicExampleDir)
+	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
+		Testing:      t,
+		TerraformDir: basicExampleDir,
+		Prefix:       "appid-bas",
+		Region:       validRegions[rand.Intn(len(validRegions))],
+	})
 
 	output, err := options.RunTestConsistency()
 	assert.Nil(t, err, "This should not have errored")
