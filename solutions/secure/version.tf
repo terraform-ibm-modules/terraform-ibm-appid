d18bd11da9d339387bd0627fe32e3bb0da0dc02a
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-06-01 08:06:11 +0100

    fix(deps): update required_provider to latest for deployable modules / architectures (#183)

--- solutions/secure/version.tf
+++ solutions/secure/version.tf
@@ -5,12 +5,12 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.65.1"
+      version = "1.66.0"
     }
     # tflint-ignore: terraform_unused_required_providers
     time = {
       source  = "hashicorp/time"
-      version = ">= 0.11.1, < 1.0.0"
+      version = ">= 0.11.2, < 1.0.0"
     }
     # tflint-ignore: terraform_unused_required_providers
     random = {
