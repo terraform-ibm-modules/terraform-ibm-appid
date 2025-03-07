22448f66c39c562cb47883636bdd98110c6665bb
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-03-07 21:09:23 +0000

    fix(deps): update required_provider to latest for deployable architectures

--- solutions/secure/version.tf
+++ solutions/secure/version.tf
@@ -5,12 +5,12 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.75.2"
+      version = "1.76.1"
     }
     # tflint-ignore: terraform_unused_required_providers
     time = {
       source  = "hashicorp/time"
-      version = ">= 0.12.1, < 1.0.0"
+      version = ">= 0.13.0, < 1.0.0"
     }
     # tflint-ignore: terraform_unused_required_providers
     random = {
