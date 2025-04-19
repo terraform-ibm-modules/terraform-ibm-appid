79d78602bdd285ede7f9be6f3afaf85b2f5e414f
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-04-19 04:09:32 +0100

    fix(deps): update terraform ibm to latest for deployable architectures (#305)

--- solutions/secure/version.tf
+++ solutions/secure/version.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.77.0"
+      version = "1.77.1"
     }
     # tflint-ignore: terraform_unused_required_providers
     time = {
